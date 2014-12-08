2aa14ae412d2028d77946a09175733f766a59713
xyresic <xyresic@crunchbang.(none)>
2014-12-07 22:30:13 -0800

    bigger banner

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -5,12 +5,6 @@
     <div class="col-lg-6 text-left">
         <h1>{{ $user->display_name }}'s Details</h1>
     </div>
-    <div class="col-lg-5 text-right">
-        Logged in as <a href="{{ route('users.show', $currentUser->id) }}">{{ $currentUser->user_name }}</a>
-    </div>
-    <div class="col-lg-1 text-left">
-        <a href="?logout">Sign Out</a>
-    </div>
 </div>
 @foreach($ridesAsDriver as $ride)
 <h5>{{ $ride->id }}</h5>
