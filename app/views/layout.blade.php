7b1233d89a2024e2fe10bd854bf58aca7b376519
MaxHampton <Max.Hampton@comcast.net>
2014-12-08 23:58:13 -0800

    Ride Detail mostly done. No editing, but everything else is in.

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,50 +1,105 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Ride Sharing and Book Selling</title>
 
         <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-
         <!-- Optional theme -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
-
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-        <link href="https://fonts.googleapis.com/css?family=Muli:400,300" rel="stylesheet" type="text/css" />
-        @yield('resources')
-        
         <script type="text/javascript">
-            window.onresize = function(event) {
-                document.getElementById("bannertitle").style.marginLeft = window.innerWidth * 0.2 + 220 + "px";
-            }
-            window.onload = function(event){
-                document.getElementById("bannertitle").style.marginLeft = window.innerWidth * 0.2 + 220 + "px";
+            function isBreakpoint( alias ) {
+                return $('.device-' + alias).is(':visible');
             }
         </script>
+
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,300" rel="stylesheet" type="text/css" />
+        @yield('resources')
     </head>
     <body>
-    
-        <div class="row" id="page-header" style="padding-bottom: 3%; background-image: url('<?php echo asset('assets/images/banner.jpg') ?>'); background-position: center; background-repeat: no-repeat; margin: 0">
-            <div class="row text-right" style="color:white; position:absolute; width: 100%; z-index: 20">
-                Logged in as <b><a href="{{ route('users.show', $currentUser->id) }}">{{ $currentUser->user_name }}</a></b>&nbsp;&nbsp;
-                <b><a href="?logout">(Logout)</a></b>
+        <div class="container-fluid">
+<!-- Header -->
+    <!-- Non-phone Header -->
+            <div class="row hidden-xs" id="page-header" style="background: url({{ Asset('assets/images/banner.jpg') }}) center no-repeat;">
+                <div class="col-xs-1">
+                    <img id="wwu-logo"
+                         src="{{asset('assets/images/westernlogo_sm_white.png')}}"
+                         style="padding: 1em;
+                         margin-left: 10%;
+                         background-color:#003f87;
+                         position: relative;
+                         z-index: 2" />
+                </div>
+                <div class="col-xs-11"
+                     style="background-color:#2b7cd1;
+                     margin: 0;
+                     position:absolute;
+                     width: 100%;
+                     top: 2em">
+                    <div class="row" id="bannertitle">
+                        <h3 class="col-xs-9" style="color: white; margin-top: 0.4em; margin-bottom: 0.4em">Western Washington University Ride Sharing</h3>
+                        <div class="col-xs-3 text-right" style="margin-top: 0.4em; margin-bottom: 0.4em">
+                            <div style="color: white; display: inline-block">
+                                Logged in as <b><a style="color: white" href="{{ route('users.show', $currentUser->id) }}">{{ $currentUser->user_name }}</a></b>
+                            </div>
+                            <div style="padding-left: 1em; text-align: right; color: white; display: inline-block">
+                                <b><a style="color: white" href="?logout">(Logout)</a></b>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <a href="/">
-                <div class="row" style="background-color:#2b7cd1; position:absolute; width: 100%; top: 2em; padding: 0; margin: 0">
-                    <h3 id="bannertitle" class="text-left" style="color: white; margin-top: 0.4em; margin-bottom: 0.4em; margin-left: 0">Western Washington University Ride Sharing</h3>
+    <!-- /Non-phone Header -->
+    <!-- Phone Header -->
+            <div class="row visible-xs">
+                <h1 class="col-xs-8">WWU Ride Sharing</h1>
+                <div class="col-xs-4 text-right">
+                    <div>
+                        Logged in as <b><a href="{{ route('users.show', $currentUser->id) }}">{{ $currentUser->user_name }}</a></b>
+                    </div>
+                    <div>
+                        <b><a href="?logout">(Logout)</a></b>
+                    </div>
                 </div>
-                <div class="col-lg-12" style="margin: 0; padding:0">    
-                    <img src="{{asset('assets/images/westernlogo_sm_white.png')}}" style="margin-left: 20%; padding: 1em; background-color:#003f87; position: relative" />
-                </div>
-            </a>
+            </div>
+    <!-- /Phone Header -->
+<!-- /Header -->
+            <div class="clearfix"></div>
+<!-- Page Content -->
+            @yield('content')
+<!-- /Page Content -->
         </div>
-        <div class="col-md-2"></div>
-        <div class="col-md-8">            
-        @yield('content')
-        @yield('footer-resources')
-        </div>
-        <div class="col-md-2"></div>
+        <!-- Divs to allow checking of screen size in JS using isBreakpoint('xs'); -->
+        <div class="device-xs visible-xs"></div>
+        <div class="device-sm visible-sm"></div>
+        <div class="device-md visible-md"></div>
+        <div class="device-lg visible-lg"></div>
     </body>
+    <script type="text/javascript">
+        function setPageHeaderOffset() {
+            var element = document.getElementById("wwu-logo");
+            var wwuLogo = $('#wwu-logo');
+            var logoRight = wwuLogo.position().left + wwuLogo.outerWidth();
+            var offset = logoRight + 20;
+
+            var docWidth = $(document).outerWidth();
+
+            document.getElementById("bannertitle").style.marginLeft = offset + "px";
+            document.getElementById("bannertitle").style.right = docWidth + "px";
+        }
+        // Window Load event. Inject JS functions in a windowLoadEvent section
+        $(window).load(function() {
+            setPageHeaderOffset();
+            @yield('windowLoadEvent')
+        });
+        $(window).resize(function(){
+            setPageHeaderOffset();
+            @yield('windowResizeEvent')
+        })
+    </script>
+    @yield('footer-resources')
 </html>
--- app/views/rides/show.blade.php
+++ app/views/rides/show.blade.php
@@ -6,129 +6,193 @@
 
 @section('content')
 <div class="row">
-    <div class="col-lg-6 text-left">
-        <h1>Ride Details</h1>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-5 well">
-        <div class="row">
-            <label class="control-label col-lg-4">Destination:</label>
-            <div class="col-lg-4 text-right">
-                {{ $ride->destination }}
-            </div>
-        </div>
-        <div class="row">
-            <label class="control-label col-lg-4">Origin:</label>
-            <div class="col-lg-4 text-right">
-                {{ $ride->origin }}
-            </div>
-        </div>
-        <div class="row">
-            <label class="control-label col-lg-4">Departure Date:</label>
-            <div class="col-lg-4 text-right">
-                <?php
-                     $date = new DateTime($ride->date);
-                     echo $date->format('m/d/y')
-                ?>
-            </div>
-        </div>
-        <div class="row">
-            <label class="control-label col-lg-4">Departure Time:</label>
-            <div class="col-lg-4 text-right">
-                <?php
-                     echo $date->format('g:i A')
-                ?>
-            </div>
-        </div>
-        <div class="row">
-            <label class="control-label col-lg-4">Driver:</label>
-            <div class="col-lg-4 text-right">
-                {{ $driver->display_name }}
-            </div>
-        </div>
-        <div class="row">
-            <label class="control-label col-lg-4">Open Seats:</label>
-            <div class="col-lg-4 text-right">
-                {{ $ride->availableSeats() }}
-            </div>
-            @if (($ride->availableSeats()>0) && ($driver->id !== $currentUser->id) && (!$ride->requestedBy($currentUser->id)))
-                {{ Form::open(array('action' => 'RideController@addRequest')) }}
-                {{ Form::hidden('rideId', $ride->id) }}
-                {{ Form::hidden('userId', $currentUser->id) }}
-                <div class="col-lg-4">
-                {{ Form::submit('Request Ride', array('class' => 'btn btn-primary')) }}
+    <div class="col-sm-6 col-xs-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <div class="panel-title">
+                    Ride Details
                 </div>
-                {{ Form::close() }}
-            @elseif(($ride->availableSeats()>0) && ($driver->id !== $currentUser->id) && ($ride->requestedBy($currentUser->id)))
-                <div class="col-lg-4">
-                    <i>Ride Requested</i>
-                </div>
-            @endif
-        </div>
-        <div class="row">
-            <label class="control-label col-lg-4">Confirmed Riders:</label>
-            @if ($passengers->count()>0)
-                <div class="col-lg-4 text-right">
-                    {{ $passengers[0]->display_name }}
-                </div>
-            @else
-                <div class="col-lg-4 text-right">
-                    <i>Available</i>
-                </div>
-            @endif
-            @for ($i = 1; $i < $passengers->count(); $i++)
+            </div>
+            <div class="panel-body">
                 <div class="row">
-                    <div class="col-lg-offset-4 col-lg-4 text-right">
-                        {{ $passengers[i]->display_name }}
+                    <label class="control-label col-xs-6 col-sm-4">Destination:</label>
+                    <div class="col-xs-6 col-sm-4 text-right">
+                        {{ $ride->destination }}
                     </div>
                 </div>
-            @endfor
-            @for ($j = 1; $j < $ride->availableSeats(); $j++)
                 <div class="row">
-                    <div class="col-lg-offset-4 col-lg-4 text-right">
-                        <i>Available</i>
+                    <label class="control-label col-xs-6 col-sm-4">Origin:</label>
+                    <div class="col-xs-6 col-sm-4 text-right">
+                        {{ $ride->origin }}
                     </div>
                 </div>
-            @endfor
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">Comments</h3>
-        </div>
-        <ul class="list-group">
-            @foreach($comments as $comment)
-            <li class="list-group-item">
                 <div class="row">
-                    <div class="col-lg-3 text-left">
-                        {{ User::find($comment->user_id)->display_name }}<br />
+                    <label class="control-label col-xs-6 col-sm-4">Departure Date:</label>
+                    <div class="col-xs-6 col-sm-4 text-right">
                         <?php
-                            $commentDate = new DateTime($comment->created_at);
-                            echo $commentDate->format('m/d/y g:i A'); ?>
+                             $date = new DateTime($ride->date);
+                             echo $date->format('m/d/y')
+                        ?>
                     </div>
-                    <p class="col-lg-9">{{ $comment->message_body }}</p>
                 </div>
-            </li>
-            @endforeach
-            <li class="list-group-item">
                 <div class="row">
-                    {{ Form::open(array('action' => 'RideController@addComment')) }}
-                    {{ Form::hidden('rideId', $ride->id) }}
-                    {{ Form::hidden('userId', $currentUser->id) }}
-                    <div class="col-lg-12">
-                        <div class="input-group">
-                            {{ Form::text('messageBody', Input::old('messageBody'), array('class' => 'form-control', 'maxlength' => '255', 'placeholder' => 'Add a comment...')) }}
-                            <span class="input-group-btn">
-                                {{ Form::submit('Submit', array('class' => "btn btn-default", 'type' => 'button')) }}
-                            </span>
+                    <label class="control-label col-xs-6 col-sm-4">Departure Time:</label>
+                    <div class="col-xs-6 col-sm-4 text-right">
+                        <?php
+                             echo $date->format('g:i A')
+                        ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <label class="control-label col-xs-6 col-sm-4">Driver:</label>
+                    <div class="col-xs-6 col-sm-4 text-right">
+                        {{ $driver->display_name }}
+                    </div>
+                </div>
+                <div class="row">
+                    <label class="control-label col-xs-6 col-sm-4">Open Seats:</label>
+                    <div class="col-xs-6 col-sm-4 text-right">
+                        {{ $ride->availableSeats() }}
+                    </div>
+                    @if (($ride->availableSeats()>0) && ($driver->id !== $currentUser->id) && (!$ride->requestedBy($currentUser->id)))
+                        {{ Form::open(array('action' => 'RideController@addRequest')) }}
+                        {{ Form::hidden('rideId', $ride->id) }}
+                        {{ Form::hidden('userId', $currentUser->id) }}
+                        <div class="col-xs-4 hidden-xs">
+                        {{ Form::submit('Request Ride', array('class' => 'btn btn-primary btn-xs')) }}
+                        </div>
+                        {{ Form::close() }}
+                    @elseif(($ride->availableSeats()>0) && ($driver->id !== $currentUser->id) && ($ride->requestedBy($currentUser->id)))
+                        <div class="col-sm-4 hidden-xs">
+                            <i>Ride Requested</i>
+                        </div>
+                    @endif
+                </div>
+                <div class="row">
+                    <label class="control-label col-xs-6 col-sm-4">Confirmed Riders:</label>
+                    @if ($passengers->count()>0)
+                        <div class="col-xs-6 col-sm-4 text-right">
+                            {{ $passengers[0]->display_name }}
+                        </div>
+                    @else
+                        <div class="col-xs-6 col-sm-4 text-right">
+                            <i>Available</i>
+                        </div>
+                    @endif
+                </div>
+                @for ($i = 1; $i < $passengers->count(); $i++)
+                    <div class="row">
+                        <div class="col-xs-offset-6 col-xs-6 col-sm-offset-4  col-sm-4 text-right">
+                            {{ $passengers[i]->display_name }}
                         </div>
                     </div>
+                @endfor
+                @for ($j = 0; $j < $ride->availableSeats(); $j++)
+                    <div class="row">
+                        <div class="col-xs-offset-6 col-xs-6 col-sm-offset-4  col-sm-4 text-right">
+                            <i>Available</i>
+                        </div>
+                    </div>
+                @endfor
+                @if (($ride->availableSeats()>0) && ($driver->id !== $currentUser->id) && (!$ride->requestedBy($currentUser->id)))
+                    <div class="row visible-xs" role="group">
+                        <div class="col-xs-12">
+                        {{ Form::open(array('action' => 'RideController@addRequest')) }}
+                        {{ Form::hidden('rideId', $ride->id) }}
+                        {{ Form::hidden('userId', $currentUser->id) }}
+                        {{ Form::submit('Request Ride', array('class' => 'btn btn-primary btn-block')) }}
+                        {{ Form::close() }}
+                        </div>
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+    @if ($driver->id === $currentUser->id)
+    <div class="col-sm-6 col-xs-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <div class="panel-title">
+                    Ride Requests
                 </div>
-            </li>
-        </ul>
+            </div>
+            @if ($requests->count()>0)
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Requester</th>
+                            <th>Request Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($requests as $request)
+                        <tr>
+                            <td>{{ $request->user->display_name }}</td>
+                            <td>
+                                <?php
+                                     $requestDate = new DateTime($request->created_at);
+                                     echo $date->format('m/d/y');
+                                ?>
+                            </td>
+                            <td>
+                                {{ Form::open(array('action' => 'RideController@confirmRequest')) }}
+                                {{ Form::hidden('rideId', $ride->id) }}
+                                {{ Form::hidden('requesterId', $request->user->id) }}
+                                {{ Form::hidden('requestId', $request->id) }}
+                                {{ Form::submit('Confirm', array('class' => 'btn btn-primary btn-xs btn-block')) }}
+                                {{ Form::close() }}
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @else
+            <div class="panel-body">
+                <i>No requests yet...</i>
+            </div>
+            @endif
+        </div>
+    </div>
+    @endif
+</div>
+<div class="clearfix"></div>
+<div class="row">
+    <div class="col-xs-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Comments</h3>
+            </div>
+            <ul class="list-group">
+                @foreach($comments as $comment)
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-4 text-left">
+                            {{ User::find($comment->user_id)->display_name }}<br />
+                            <?php
+                                $commentDate = new DateTime($comment->created_at);
+                                echo $commentDate->format('m/d/y g:i A'); ?>
+                        </div>
+                        <p class="col-xs-8">{{ $comment->message_body }}</p>
+                    </div>
+                </li>
+                @endforeach
+                <li class="list-group-item">
+                    <div class="row">
+                        {{ Form::open(array('action' => 'RideController@addComment')) }}
+                        {{ Form::hidden('rideId', $ride->id) }}
+                        {{ Form::hidden('userId', $currentUser->id) }}
+                        <div class="col-lg-12">
+                            <div class="input-group">
+                                {{ Form::text('messageBody', Input::old('messageBody'), array('class' => 'form-control', 'maxlength' => '255', 'placeholder' => 'Add a comment...')) }}
+                                <span class="input-group-btn">
+                                    {{ Form::submit('Submit', array('class' => "btn btn-default", 'type' => 'button')) }}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
 </div>
 @stop
